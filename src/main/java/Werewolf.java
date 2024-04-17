a1987d84d771567278f737f26965464ca07daee4
Maxim Turbal <turbalmaxim@gmail.com>
2024-04-17 19:17:36 +0300

    done

--- src/main/java/Werewolf.java
+++ src/main/java/Werewolf.java
@@ -5,7 +5,7 @@
 * If the value is false, return "Howl!"
 * It's important that the returned values are exactly as specified so that the tests pass successfully. */
 
-public class Werewolf extends Mutant implements Animal, Human{
+public class Werewolf extends Mutant implements Animal, Human {
     public boolean isHumanForm;
 
     public Werewolf() {
@@ -23,11 +23,19 @@ public class Werewolf extends Mutant implements Animal, Human{
 
     @Override
     public String sound() {
-        return null;
+        if (isHumanForm) {
+            return "No sound in human form.";
+        } else {
+            return "Howl!";
+        }
     }
 
     @Override
     public String speak() {
-        return null;
+        if (isHumanForm) {
+            return "I am a human.";
+        } else {
+            return "I cannot speak in wolf form.";
+        }
     }
 }
